3a1e526d1681734545b33f714819457c7c389bfe
MikhaelPA <mikhael.oda@gmail.com>
2019-09-18 21:49:50 +0700

    Add project files.

---
+++ ApiSupport/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSupport.Controllers
+{
+    [Route("sample")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok($"This works too on {DateTime.Now.ToShortDateString()}");
+        }
+
+        // GET api/values/5
+        [HttpGet("{param}")]
+        public ActionResult<string> Get(string param)
+        {
+            return Ok($"This works too {param}");
+        }
+    }
+}
